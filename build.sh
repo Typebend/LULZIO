0ecf9bae45eee7dea556cef9acb313a8dad44f21
Jacob Barber <jacob.barber@bamtechmedia.com>
2018-08-07 16:01:13 -0400

    Improve compiler, add travis build

--- build.sh
+++ build.sh
@@ -11,6 +11,23 @@ echo ''
 echo '                                      The Greatest IO Monad'
 echo ''
 
-
-
-exec ./compiler/lulzio-compiler
+if [ ! -f ./compiler/lulzio-compiler ]; then
+  if [ -x $(which ghc) ]; then
+    echo "Building LULZIO compiler."
+    ./compiler/bootstrap
+    ./build.sh
+  else
+    echo "No GHC installation found. Proceeding with SLOW, UNOPTIMIZED compiler."
+    echo "You really should consider using a more principled build system."
+    echo "I would strongly enocourage you to install the Glasgow Haskell Compiler;"
+    echo "Your quality of life will drastically improve, and you'll be able to"
+    echo "enjoy OPTIMIZED LULZIO performance."
+    sbt compile
+  fi
+else
+  ./compiler/lulzio-compiler
+  if [ $? -ne 0 ]; then
+    rm ./compiler/lulzio-compile
+    ./build.sh
+  fi
+fi
